79907effd753741c11bbfaf4213facbbaa75cf53
Eric Griffis <dedbox@gmail.com>
2025-08-14 13:00:53 -0700

    add input polling

---
+++ Runic/src/Platform/Linux/LinuxInput.cpp
@@ -0,0 +1,34 @@
+#include "Platform/Linux/LinuxInput.hpp"
+
+#include "GLFW/glfw3.h"
+#include "Runic/Application.hpp"
+
+namespace Runic
+{
+Input* Input::s_instance{new LinuxInput};
+
+bool LinuxInput::isKeyPressedImpl(const int keyCode)
+{
+    const auto window{static_cast<GLFWwindow*>(Application::get().getWindow().getNativeWindow())};
+    const auto state{glfwGetKey(window, keyCode)};
+
+    return state == GLFW_PRESS || state == GLFW_REPEAT;
+}
+
+bool LinuxInput::isMouseButtonPressedImpl(const int button)
+{
+    const auto window{static_cast<GLFWwindow*>(Application::get().getWindow().getNativeWindow())};
+    const auto state{glfwGetMouseButton(window, button)};
+
+    return state == GLFW_PRESS;
+}
+
+std::pair<float, float> LinuxInput::getMousePositionImpl()
+{
+    const auto window{static_cast<GLFWwindow*>(Application::get().getWindow().getNativeWindow())};
+    double xPos, yPos;
+    glfwGetCursorPos(window, &xPos, &yPos);
+
+    return {static_cast<float>(xPos), static_cast<float>(yPos)};
+}
+} // Runic
---
+++ Runic/src/Platform/Linux/LinuxInput.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Runic/Input.hpp"
+
+namespace Runic
+{
+class LinuxInput : public Input
+{
+public:
+    bool isKeyPressedImpl(int keyCode) override;
+
+    bool isMouseButtonPressedImpl(int button) override;
+
+    std::pair<float, float> getMousePositionImpl() override;
+};
+} // Runic
