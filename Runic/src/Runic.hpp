e2c7fc7558461dce6f3a31b7ddd794c52708c778
Eric Griffis <dedbox@gmail.com>
2025-08-14 14:35:05 -0700

    add custom key and mouse button codes

--- Runic/src/Runic.hpp
+++ Runic/src/Runic.hpp
@@ -2,9 +2,14 @@
 
 // Includes for Runic applications
 #include <Runic/Application.hpp>
-#include <Runic/ImGui/ImGuiLayer.hpp>
-#include <Runic/Layer.hpp>
 #include <Runic/Log.hpp>
+
+#include <Runic/Layer.hpp>
+#include <Runic/ImGui/ImGuiLayer.hpp>
+
+#include <Runic/Input.hpp>
+#include <Runic/KeyCode.hpp>
+#include <Runic/MouseButtonCode.hpp>
 
 // Main Entry Point
 #include <Runic/Main.hpp>
---
+++ Runic/src/Runic/MouseButtonCode.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Runic
+{
+// From glfw3.h
+enum class MouseButton
+{
+    Button_1      = 0,
+    Button_2      = 1,
+    Button_3      = 2,
+    Button_4      = 3,
+    Button_5      = 4,
+    Button_6      = 5,
+    Button_7      = 6,
+    Button_8      = 7,
+    Button_Left   = Button_1,
+    Button_Right  = Button_2,
+    Button_Middle = Button_3,
+};
+}
