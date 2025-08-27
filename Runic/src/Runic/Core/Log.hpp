22542f021f420973197ca5fc85a11f3853e259c9
Eric Griffis <dedbox@gmail.com>
2025-08-26 22:08:08 -0700

    enable debug build detection in Log calls

--- Runic/src/Runic/Core/Log.hpp
+++ Runic/src/Runic/Core/Log.hpp
@@ -30,31 +30,31 @@ constexpr bool Debugging = true;
 template <typename Fmt, typename... Args>
 void Trace(Fmt fmt, Args... args)
 {
-    if (Debugging) ::Runic::Log::Core()->trace(fmt::runtime(fmt), args...);
+    if constexpr (Debugging) ::Runic::Log::Core()->trace(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Info(Fmt fmt, Args... args)
 {
-    if (Debugging) ::Runic::Log::Core()->info(fmt::runtime(fmt), args...);
+    if constexpr (Debugging) ::Runic::Log::Core()->info(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Warn(Fmt fmt, Args... args)
 {
-    if (Debugging) ::Runic::Log::Core()->warn(fmt::runtime(fmt), args...);
+    if constexpr (Debugging) ::Runic::Log::Core()->warn(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Error(Fmt fmt, Args... args)
 {
-    if (Debugging) ::Runic::Log::Core()->error(fmt::runtime(fmt), args...);
+    if constexpr (Debugging) ::Runic::Log::Core()->error(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Critical(Fmt fmt, Args... args)
 {
-    if (Debugging) ::Runic::Log::Core()->critical(fmt::runtime(fmt), args...);
+    if constexpr (Debugging) ::Runic::Log::Core()->critical(fmt::runtime(fmt), args...);
 }
 
 } // namespace Core
@@ -62,31 +62,31 @@ void Critical(Fmt fmt, Args... args)
 template <typename Fmt, typename... Args>
 void Trace(Fmt fmt, Args... args)
 {
-    if (Core::Debugging) ::Runic::Log::Client()->trace(fmt::runtime(fmt), args...);
+    if constexpr (Core::Debugging) ::Runic::Log::Client()->trace(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Info(Fmt fmt, Args... args)
 {
-    if (Core::Debugging) ::Runic::Log::Client()->info(fmt::runtime(fmt), args...);
+    if constexpr (Core::Debugging) ::Runic::Log::Client()->info(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Warn(Fmt fmt, Args... args)
 {
-    if (Core::Debugging) ::Runic::Log::Client()->warn(fmt::runtime(fmt), args...);
+    if constexpr (Core::Debugging) ::Runic::Log::Client()->warn(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Error(Fmt fmt, Args... args)
 {
-    if (Core::Debugging) ::Runic::Log::Client()->error(fmt::runtime(fmt), args...);
+    if constexpr (Core::Debugging) ::Runic::Log::Client()->error(fmt::runtime(fmt), args...);
 }
 
 template <typename Fmt, typename... Args>
 void Critical(Fmt fmt, Args... args)
 {
-    if (Core::Debugging) ::Runic::Log::Client()->critical(fmt::runtime(fmt), args...);
+    if constexpr (Core::Debugging) ::Runic::Log::Client()->critical(fmt::runtime(fmt), args...);
 }
 
 } // namespace Runic
