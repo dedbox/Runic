5dd75c14de772311567984b860f7bc69a0716334
Eric Griffis <dedbox@gmail.com>
2025-08-27 17:24:10 -0700

    simplify logging functions' template paramters (still hacky)
    also changes Assert log level to critical

--- Runic/src/Runic/Core/Log.hpp
+++ Runic/src/Runic/Core/Log.hpp
@@ -27,43 +27,44 @@ constexpr bool Debugging = false;
 constexpr bool Debugging = true;
 #endif
 
-template <typename Fmt, typename... Args>
-void Trace(Fmt fmt, Args... args)
+template <typename... Args>
+void Trace(const std::string& fmt, Args... args)
 {
     if constexpr (Debugging) ::Runic::Log::Core()->trace(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Info(Fmt fmt, Args... args)
+template <typename... Args>
+void Info(const std::string& fmt, Args... args)
 {
     if constexpr (Debugging) ::Runic::Log::Core()->info(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Warn(Fmt fmt, Args... args)
+template <typename... Args>
+void Warn(const std::string& fmt, Args... args)
 {
     if constexpr (Debugging) ::Runic::Log::Core()->warn(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Error(Fmt fmt, Args... args)
+template <typename... Args>
+void Error(const std::string& fmt, Args... args)
 {
     if constexpr (Debugging) ::Runic::Log::Core()->error(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Critical(Fmt fmt, Args... args)
+template <typename... Args>
+void Critical(const std::string& fmt, Args... args)
 {
     if constexpr (Debugging) ::Runic::Log::Core()->critical(fmt::runtime(fmt), args...);
 }
 
-constexpr void Assert(bool test, const std::string& errMsg)
+template <typename... Args>
+constexpr void Assert(bool test, const std::string& fmt, Args... args)
 {
     if constexpr (Debugging)
     {
         if (!test)
         {
-            Error("Assertion failed: {}", errMsg);
+            Critical("Assertion failed: " + fmt, args...);
             if (!raise(SIGTRAP)) abort();
         }
     }
@@ -71,43 +72,44 @@ constexpr void Assert(bool test, const std::string& errMsg)
 
 } // namespace Core
 
-template <typename Fmt, typename... Args>
-void Trace(Fmt fmt, Args... args)
+template <typename... Args>
+void Trace(const std::string& fmt, Args... args)
 {
     if constexpr (Core::Debugging) ::Runic::Log::Client()->trace(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Info(Fmt fmt, Args... args)
+template <typename... Args>
+void Info(const std::string& fmt, Args... args)
 {
     if constexpr (Core::Debugging) ::Runic::Log::Client()->info(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Warn(Fmt fmt, Args... args)
+template <typename... Args>
+void Warn(const std::string& fmt, Args... args)
 {
     if constexpr (Core::Debugging) ::Runic::Log::Client()->warn(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Error(Fmt fmt, Args... args)
+template <typename... Args>
+void Error(const std::string& fmt, Args... args)
 {
     if constexpr (Core::Debugging) ::Runic::Log::Client()->error(fmt::runtime(fmt), args...);
 }
 
-template <typename Fmt, typename... Args>
-void Critical(Fmt fmt, Args... args)
+template <typename... Args>
+void Critical(const std::string& fmt, Args... args)
 {
     if constexpr (Core::Debugging) ::Runic::Log::Client()->critical(fmt::runtime(fmt), args...);
 }
 
-constexpr void Assert(bool test, const std::string& errMsg)
+template <typename... Args>
+constexpr void Assert(bool test, const std::string& fmt, Args... args)
 {
     if constexpr (Core::Debugging)
     {
         if (!test)
         {
-            Error("Assertion failed: {}", errMsg);
+            Critical("Assertion failed: " + fmt, args...);
             if (!raise(SIGTRAP)) abort();
         }
     }
