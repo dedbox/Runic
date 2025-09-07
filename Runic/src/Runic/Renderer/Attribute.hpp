ca2ce9991578a5e4e08db275bfbcde2ad27eddbc
Eric Griffis <dedbox@gmail.com>
2025-09-06 17:30:26 -0700

    add basic compile-time Attribute, AttributeLayout abstractions

---
+++ Runic/src/Runic/Renderer/Attribute.hpp
@@ -0,0 +1,155 @@
+#pragma once
+
+#include "Runic/Renderer/GraphicsContext.hpp"
+
+namespace Runic
+{
+
+// Vertex Attribute --------------------------------------------------------------------------------
+
+/** Constrains an Attribute component count according to its type. */
+template <typename T, size_t N>
+concept ValidAttributeParams = (
+    // clang-format off
+    (std::same_as<T, bool> && N == 1) ||
+    (std::same_as<T, int>  && N >= 1 && N <= 4) ||
+    (std::same_as<T, float> && (N >= 1 && N <= 4) || N == 9 || N == 16)
+    // clang-format on
+);
+
+/** A purely compile-time vertex attribute abstraction
+ *
+ * Attributes are essentially phantom type tags for vertex data sources. These tags enable automatic
+ * generation of vertex buffer layout code, and provide a means for ensuring Mesh and ShaderProgram
+ * objects use the same vertex buffer formats at compile time.
+ *
+ * Each Attribute declares a type and number of individual components; for example, a vertex
+ * consisting of a three-dimensional position and a pair of UV texture coordinates would use two
+ * Attribute tags. Assuming all coordinates are normalized floating-point values, they are:
+ *
+ * 1. Attribute<float, 3> (a three-dimensional position)
+ * 2. Attribute<float, 2> (a pair of UV texture coordinates)
+ */
+template <typename T, size_t N, bool B = false>
+    requires(ValidAttributeParams<T, N>)
+struct Attribute
+{
+    using type = T; /**< The underlying type */
+
+    static constexpr size_t count   = N; /**< The number of components of type T */
+    static constexpr bool normalize = B; /**< Whether the components need to be normalized */
+    static constexpr size_t size    = N * sizeof(T); /**< The size in bytes of all components */
+};
+
+/** Ensures the template parameters of an Attribute instance are properly constrained. */
+template <typename Attr>
+concept ValidAttribute = (ValidAttributeParams<typename Attr::type, Attr::count>);
+
+// Vertex Attribute Type ---------------------------------------------------------------------------
+
+/** Converts an Attribute to an AttributeType. */
+template <typename Attr>
+constexpr AttributeType to_AttributeType()
+{
+    if constexpr (std::same_as<typename Attr::type, bool>)
+        return AttributeType::Bool;
+
+    else if constexpr (std::same_as<typename Attr::type, int>)
+    {
+        // clang-format off
+        switch (Attr::count)
+        {
+        case 1: return AttributeType::Int;
+        case 2: return AttributeType::Int2;
+        case 3: return AttributeType::Int3;
+        case 4: return AttributeType::Int4;
+        }
+        // clang-format on
+    }
+
+    else if constexpr (std::same_as<typename Attr::type, float>)
+    {
+        // clang-format off
+        switch (Attr::count)
+        {
+        case 1: return AttributeType::Float;
+        case 2: return AttributeType::Float2;
+        case 3: return AttributeType::Float3;
+        case 4: return AttributeType::Float4;
+        case 9: return AttributeType::Mat3;
+        case 16: return AttributeType::Mat4;
+        }
+        // clang-format on
+    }
+}
+
+// Vertex Attribute Layout -------------------------------------------------------------------------
+
+/** Constrains a parameter pack to Attributes with a common underlying type. */
+template <typename Attr, typename... Attrs>
+concept SameAttrType = (std::same_as<typename Attr::type, typename Attrs::type> && ...);
+
+/** Ensures all Attribute declarations are valid and have a common underlying type. */
+template <typename Attr, typename... Attrs>
+concept ValidLayoutAttributes = requires {
+    requires ValidAttribute<Attr>;
+    requires(ValidAttribute<Attrs> && ...);
+    requires SameAttrType<Attr, Attrs...>;
+};
+
+/** A compile-time collection of Attribute tags */
+template <typename Attr, typename... Attrs>
+    requires ValidLayoutAttributes<Attr, Attrs...>
+struct Layout
+{
+    /** The common underlying type */
+    using type = Attr::type;
+
+    /** The total number of components in all Attributes */
+    static constexpr size_t component_count = Attr::count + (Attrs::count + ...);
+
+    /** The number of Attributes */
+    static constexpr size_t attr_count = 1 + sizeof...(Attrs);
+
+    /** The size in bytes of all attributes */
+    static constexpr size_t stride = Attr::size + (Attrs::size + ...);
+
+    /** The component count of each Attribute */
+    static constexpr std::array<size_t, attr_count> counts{Attr::count, Attrs::count...};
+
+    /** The size in bytes of each Attribute */
+    static constexpr std::array<size_t, attr_count> sizes{Attr::size, Attrs::size...};
+
+    static constexpr std::array<AttributeType, attr_count> attributeTypes{
+        to_AttributeType<Attr>(), to_AttributeType<Attrs>()...};
+
+    /** The byte index of the beginning of each Attribute */
+    static constexpr std::array<size_t, attr_count> offsets = []() {
+        std::array<size_t, attr_count> result{};
+        result[0]  = 0;
+        int offset = 0;
+        for (size_t i = 1; i < attr_count; i++)
+        {
+            offset += sizes[i - 1];
+            result[i] = offset;
+        }
+        return result;
+    }();
+};
+
+template <typename T>
+struct ValidLayoutImpl
+{
+};
+
+template <typename A, typename... As>
+struct ValidLayoutImpl<Layout<A, As...>>
+{
+    static constexpr bool value = ValidLayoutAttributes<A, As...>;
+};
+
+/** Ensures the template parameters of an AttributeLayout instance are properly constrained. */
+template <typename T>
+concept ValidLayout = ValidLayoutImpl<T>::value;
+
+} // namespace Runic
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -5,6 +5,9 @@
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/trigonometric.hpp"
 
+#include "Runic/Renderer/Attribute.hpp"
+#include "Runic/Renderer/GraphicsContext.hpp"
+
 static constexpr auto Bg = Runic::color(0.2F, 0.3F, 0.3F, 1.0F);
 
 static constexpr auto Taupe  = Runic::Color::hex(0x463F3AFF);
@@ -68,6 +71,44 @@ public:
             -0.5F,  0.5F, -0.5F,  0.0F, 1.0F,
         };
         // clang-format on
+
+        using APos3 = Runic::Attribute<float, 3>;
+        using ACol4 = Runic::Attribute<float, 4>;
+        using ATex2 = Runic::Attribute<float, 2>;
+
+        using LPos3Color4Tex2 = Runic::AttributeLayout<APos3, ACol4, ATex2>;
+
+        std::println(
+            "Pos3 {} {} {} {}", sizeof(APos3), APos3::count, APos3::normalize, APos3::size);
+        std::println(
+            "Col4 {} {} {} {}", sizeof(ACol4), ACol4::count, ACol4::normalize, ACol4::size);
+        std::println(
+            "Tex2 {} {} {} {}", sizeof(ATex2), ATex2::count, ATex2::normalize, ATex2::size);
+        std::println("Pos3Tex2 {} {}", LPos3Color4Tex2::count, LPos3Color4Tex2::stride);
+        std::println(
+            "counts {}",
+            LPos3Color4Tex2::counts | std::ranges::views::transform([](const size_t& count) {
+                return std::to_string(count);
+            }) | std::ranges::views::join_with(' ') |
+                std::ranges::to<std::string>());
+        std::println(
+            "sizes {}",
+            LPos3Color4Tex2::sizes | std::ranges::views::transform([](const size_t& size) {
+                return std::to_string(size);
+            }) | std::ranges::views::join_with(' ') |
+                std::ranges::to<std::string>());
+        std::println(
+            "types {}",
+            LPos3Color4Tex2::attributeTypes |
+                std::ranges::views::transform(
+                    [](const Runic::AttributeType& type) { return to_string(type); }) |
+                std::ranges::views::join_with(' ') | std::ranges::to<std::string>());
+        std::println(
+            "offsets {}",
+            LPos3Color4Tex2::offsets | std::ranges::views::transform([](const size_t& offset) {
+                return std::to_string(offset);
+            }) | std::ranges::views::join_with(' ') |
+                std::ranges::to<std::string>());
 
         const std::vector<Runic::VertexAttribute> layout = {
             {.type = Runic::AttributeType::Float3, .normalize = false}, // a_Position
@@ -241,8 +282,7 @@ class Sandbox : public Runic::Application
 {
 public:
     Sandbox()
-        // : Application({.name = "Sandbox"})
-        : Application({.name = "Sandbox"}, {.resizePolicy = Runic::FixedAspect(16.0F / 9.0F)})
+        : Application({.name = "Sandbox"})
     {
         Runic::Log::SetLevel(spdlog::level::info);
 
